f322a400a2c1b8ca6a1a5f66194ff9213c64ea8b
Maithili Gavli <83765226+Maithili1905@users.noreply.github.com>
2021-05-08 16:26:33 +0530

    Create prog25.java

---
+++ prog25.java
@@ -0,0 +1,57 @@
+/*2. Write Java program which accept number of rows and number of columns
+from user and display below pattern.
+Input : iRow = 4 iCol = 4
+Output : A B C D
+         a b c d
+         A B C D
+         a b c d */
+		 
+
+import java.lang.*;
+import java.util.*;
+
+
+  class Matrix
+  {
+	  void Display(int row , int col)
+	  {
+		  int i = 0 , j = 0;
+		  int ch = 65 ;
+		  int c = 97;
+		  
+		  for(i = 0; i < row; i++)
+		  {
+			  for(j = 0 ; j < col ;j++)
+			  {
+				  if(i % 2 == 0)
+				  {
+					  System.out.print((char) (c+j)+ " ");
+				  }
+				  else
+				  {
+					  System.out.print((char) (ch+j)+ " " );
+				  }
+			  }
+			  System.out.println();
+		  }
+	  }
+  }
+  
+ class demo
+ {
+	 public static void main(String arg[])
+	 {
+		 Scanner sobj = new Scanner(System.in);
+		 
+		 System.out.println("enter value for rows");
+		 int row = sobj.nextInt();
+		 
+		 
+		 System.out.println("enter value for columns");
+		 int col = sobj.nextInt();
+		 
+		 Matrix mobj = new Matrix();
+		 
+		 mobj.Display(row,col);
+	 }
+ }
